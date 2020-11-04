919d75c7fdc9cdd9a78185e4101ab80a4dfae699
unknown <bryantvincken@gmail.com>
2020-11-04 18:04:07 +0100

    Test voor arduino gemaakt om seriële verbinding te testen

---
+++ C_Tests/Arduino_Serial_C.c
@@ -0,0 +1,89 @@
+/*
+ * ontvangViaSerial.c
+ *
+ * Created: 4-11-2020 16:36:31
+ * Author : Echo
+ 
+ Code om ledjes te laten branden op een arduino middels input die je ingeeft via de terminal
+ De Arduino ontvangt data op het RX kanaal van de Serial en leest deze uit
+ Aan de hand van de binnengekomen data kijkt de arduino in de while loop welke actie hij moet uitvoeren. 
+ */ 
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/delay.h>
+
+#define F_CPU 16000000 //kloksnelheid CPU
+#define BAUD 9600 //maximum frequentie bits per seconde
+#define BRC ((F_CPU/16/BAUD) -1) //Baud rate calculator = 16 bits
+#define RX_BUFFER_SIZE 128
+
+char rxBuffer[RX_BUFFER_SIZE]; //rxBuffer heeft lijst met 128 lijnen
+uint8_t rxReadPos = 0;
+uint8_t rxWritePos = 0;
+
+char getChar(void);
+char peekChar(void);
+
+int main(void)
+{
+    UBRR0H = (BRC >> 8); //High byte shift 8 plaatsen om de hoogste 8 bits te pakken en plaatst deze in baudrate registerH
+    UBRR0L = (BRC);      //Lower byte is BRC, de laagste 8 bits en plaatst deze in baudrate registerL
+	
+	UCSR0B = (1 << RXEN0) | (1 << RXCIE0); //"UCSR0B = USART Control and Status Register B" //Zet de receiver aan = "RX op arduino", en set bit for interrupt enable, elke nieuwe data die binnenkomt triggert de interrupt
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); //Zet de character size die de receiver kan gebruiken // 8 bits
+	
+	DDRB = 0b00001111; //Zet DDRB port 8, 9, 10 en 11 op output
+	sei(); //zet external interrupts aan
+	
+	
+    while (1) 
+    {
+		char c = getChar(); //Haalt karakter op van serial bus
+		
+		if(c == '1'){
+			PORTB ^= 0b00000001; //Zet de LED aan
+		} else if(c == '2'){
+			PORTB = 0b00000010;
+		} else if (c == '3'){
+			PORTB ^= 0b00000100;
+		} else if(c == '4'){
+			PORTB ^= 0b00001000;;
+		} else if (c == '0'){
+			PORTB = 0x00;
+		}
+    }
+}
+
+char peekChar(void){ //Returnt het eerst volgende karakter dat gelezen gaat worden
+	char ret = '\0';
+	
+	if(rxReadPos != rxWritePos){
+		ret = rxBuffer[rxReadPos];
+	}
+	return ret;
+}
+
+char getChar(void){ //Haalt karakter op dat moet worden gelezen en increment de leespositie
+	char ret = '\0';
+	
+	if(rxReadPos != rxWritePos){ //Als er nieuwe data is maar de readposition staat achter op de 
+		ret = rxBuffer[rxReadPos];
+		rxReadPos++;
+		
+		if(rxReadPos >= RX_BUFFER_SIZE){
+			rxReadPos = 0;
+		}
+		return ret;
+	}
+}
+ISR(USART_RX_vect){  //De interrupt
+	
+	rxBuffer[rxWritePos] = UDR0; //UDR0 is wat er in gelezen word vanaf serial
+	rxWritePos++;
+	
+	if(rxWritePos >= RX_BUFFER_SIZE){ // Als de buffer van 128 vol is
+		rxWritePos = 0; //rxWritePos is 0
+	}
+}
