458f686e7f9a0f326f3ee539dd94259041e4bb00
unknown <bryantvincken@gmail.com>
2020-11-04 11:41:04 +0100

    Comments bij main gedaan

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import serial
 
-ser = serial.Serial('COM3', baudrate = 9600, timeout=1)
+ser = serial.Serial('COM3', baudrate = 9600, timeout=1) #Start een verbinding op COM3 met een baudrate van 9600 
 
-def getValues():
-     ser.write(b'g')
-     arduinoData = ser.readline().decode('ascii')
-     return arduinoData
+def getValues(): #Request, read and return data from Arduino
+     ser.write(b'g') #Verzend een byte naar serial bus, en stuur letter 'g'
+     arduinoData = ser.readline().decode('ascii') #lees lijn die binnenkomt vanaf arduino en decode deze naar ascii
+     return arduinoData 
 
 while(1):
 
