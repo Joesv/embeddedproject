2573fd56cfb49e5825e3ab05f36c94ad398568a9
unknown <bryantvincken@gmail.com>
2020-11-08 13:09:17 +0100

    Python communicatie over serial naar Arduino C werkend

--- Centrale - Python/CentraleUnit.py
+++ Centrale - Python/CentraleUnit.py
@@ -6,7 +6,7 @@ import binascii
 class CentraleUnit: 
     def __init__(self, COMport, typemodel):
         self.COMport = COMport
-        self.serialCon = serial.Serial(port=self.COMport, baudrate = 9600)
+        self.serialCon = serial.Serial(port=self.COMport, baudrate = 9600, timeout=2) #//Timeout op 2 laten staan, dit geeft de arduino genoeg denktijd om python the processen
         self.typemodel = typemodel
 
     def getCOMPort(self):
@@ -18,8 +18,15 @@ class CentraleUnit:
     def getTypeModel(self):
         return self.typemodel
 
-    def sendcommand(self):
-        self.serialCon.write(bytes([1]))
+    def sendcommand(self, n): #// n = cijfer dat een actie triggert in de c code op de arduino
+        self.serialCon.write(n.encode())
 
-arduino = CentraleUnit("COM4", "jaweekveel")
-arduino.sendcommand()
+    def readCommand(self): #// readcommand is nodig om correct een byte te versturen. Zonder daanroep van deze methode werkt het versturen op serial niet. 
+        self.serialCon.readline()
+
+    
+
+
+arduino = CentraleUnit("COM4", "TempSensor") #//Maakt een CentraleUnit object met een verbinding via COM4 met als typenaam TempSensor
+arduino.readCommand() #//Readcommand is nodig om sendCommand correct te gebruiken
+arduino.sendcommand('2') #//Verstuurt '2' als byte over de serial port naar de arduino
